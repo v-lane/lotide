f4cffea9ed8b9da4a79c23054dccabe7a78cd055
v-lane <victoria.lane.m@gmail.com>
2024-02-05 18:16:06 -0500

    Created function assertEqual in lotide

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//test code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("12", 12);
+assertEqual(true, "true");
+assertEqual("apple","paple");
+assertEqual(11,1);
+assertEqual("apple","apple");
+
