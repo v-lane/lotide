ffe26fe2eaab39845c415b3b0a921e26072445e8
v-lane <victoria.lane.m@gmail.com>
2024-02-05 19:34:57 -0500

    added function 'head' to return 1st value in array

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+}
+
+
+//test code
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([],undefined));
+assertEqual(head([1]),1);
+assertEqual(head([1,2,3]),1);
+assertEqual(head(["1","2"]),"1");
