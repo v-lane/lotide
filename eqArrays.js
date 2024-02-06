7aaf05ae518e41f7583002867751bd22ec866908
v-lane <victoria.lane.m@gmail.com>
2024-02-06 18:39:17 -0500

    added new test case

--- eqArrays.js
+++ eqArrays.js
@@ -29,10 +29,11 @@ let eqArrays = function(arr1, arr2) {
 //tests
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["true", "2", "3"], [true, "2", 3]), false);
 assertEqual(eqArrays(["true", "false", "happy"], ["true", "FALSE", "happy"]), false);
+assertEqual(eqArrays([],[]), true);
+
