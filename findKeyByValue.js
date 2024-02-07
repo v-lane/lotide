60ad63af0086d9751d2582b11884e7cae382f2b5
v-lane <victoria.lane.m@gmail.com>
2024-02-07 18:06:40 -0500

    created function

---
+++ findKeyByValue.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// implement function 'findKeyByValue'
+// take object and value
+// scan object, return first key which contains given value
+// if no key found with value, return undefined
+
+
+const findKeyByValue = function(object, value) {
+  let result = undefined;
+
+  for (let key in object) {
+    if (object[key] === value) {
+      result = key;
+      break;
+    }
+  }
+  return result;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  crime: "The Wire",
+  drama: "The Wire"
+};
+
+//tests
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "crime");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "THE WIRE"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Simpsons"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, 12), undefined);
