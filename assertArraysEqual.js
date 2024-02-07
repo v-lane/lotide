4a7d4c684f4b287e520ac6dbfdead86920356061
v-lane <victoria.lane.m@gmail.com>
2024-02-06 19:09:31 -0500

    minor semantic change re LL AI feedback

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -17,7 +17,7 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   let pass = eqArrays(arr1, arr2);
-  if (pass === true) {
+  if (pass) {
     console.log(`✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
